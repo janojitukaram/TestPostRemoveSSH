8e04ee0a5bcc499bc41aff24e038d895ef05df0e
janojitukaram <166199972+janojitukaram@users.noreply.github.com>
2024-05-10 16:12:40 +0530

    Update ListenersDemo.java

--- src/test/java/listeners/ListenersDemo.java
+++ src/test/java/listeners/ListenersDemo.java
@@ -3,7 +3,7 @@ package listeners;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-
+//added comment for testing
 public class ListenersDemo implements ITestListener{
 
 	public void onTestStart(ITestResult result) {
